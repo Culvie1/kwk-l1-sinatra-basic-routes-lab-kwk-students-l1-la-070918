195739c9d273e707694e31d6dab5c08c1cf60119
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-17 23:14:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 end
 
+class App
+  get '/' do
+    "Hello World"
+  end
+end
+
 get "/:name" do
     @name = params[:name]
     "My name is #{@name}."
