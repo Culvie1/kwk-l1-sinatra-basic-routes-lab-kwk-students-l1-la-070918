8dc7c48aee6dd4ade6b6fbf636b2858d23bc4346
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-17 23:13:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,13 +6,12 @@ end
 get "/:name" do
     @name = params[:name]
     "My name is #{@name}."
-    
+    end 
 get "/:hometown" do
     @hometown = params[:hometown]
     "My hometwon is #{@hometown}."
-
+    end 
 get "/:song" do
     @song = params[:song]
     "My favorite song is #{@song}."
- 
-  end
+    end
