fd1e58a873d6462980cf4de448c20f9a157da9ed
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-17 23:09:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,16 +4,16 @@ class App < Sinatra::Base
 end
 
 get "/hello/:name" do
-    @user_name = params[:name]
-    "My name is #{@user_name}."
+    @name = params[:name]
+    "My name is #{@name}."
     
 get "/hello/:name" do
     @hometown = params[:name]
-    "My hometwon is #{hometown}."
+    "My hometwon is #{@hometown}."
 
 get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "My favorite song is #{@song}."
   end
 
 
