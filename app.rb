b27b677170bad5f8110c868cc30ea6019760fd7f
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-17 23:07:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,12 @@ end
 
 get "/hello/:name" do
     @user_name = params[:name]
+    "My name is #{@user_name}!"
+    
+get "/hello/:name" do
+    @user_name = params[:name]
     "Hello #{@user_name}!"
+  end
+
+  end 
   end
